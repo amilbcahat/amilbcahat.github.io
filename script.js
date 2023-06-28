f053dea798aed8a1b5bee0f332246eea4f8e3a9b
amilbcahat <68186313+amilbcahat@users.noreply.github.com>
2023-06-28 21:37:19 +0530

    initial

---
+++ script.js
@@ -0,0 +1,74 @@
+const BLACKLISTED_KEY_CODES = [ 38 ];
+const COMMANDS = {
+  help:
+    'Supported commands: <span class="code">about</span>, <span class="code">experience</span>, <span class="code">education</span>, <span class="code">skills</span>, <span class="code">certifications</span>, <span class="code">contact</span>',
+  about:
+    "Hello I'm Amol Verma also known by alias Mr.Shebang or Dingus, a cyber security researcher with a lot of potential. Currently I am pursing my Bachelor's degree in Information Technology. I am well-equipped to identify and analyze potential security threats, vulnerabilites. I am excited to continue leveraging my skills and experience to help organizations stay ahead of potential cyber threats and protect their valuable assets.",
+  skills:
+    "Ethical Hacking | Penetration Testing | Red Teaming | Offensive Testing | Full Stack Developer | Bug Bounty Hunter",
+  education:
+    " <br> Currently Pursuing BTech in Information Technology ",
+  experience:'<span class="code"> Working as a CISO and COO at Acadhut </span><br> Security Researcher | Bug Hunter on the Various Platforms - Present<br> ',
+  certifications: 
+    " MCEH - Metaxone Certified Ethical Hacker ",
+  contact:
+    "You can contact me on Linkedin and or via the mail:<br><a href='https://www.linkedin.com/in/zingus/' class='success link'>Linkedin</a>, <a href='mailto:amolverma.246@gmail.com' class='success link'>Email</a>,"
+};
+let userInput, terminalOutput;
+
+const app = () => {
+  userInput = document.getElementById("userInput");
+  terminalOutput = document.getElementById("terminalOutput");
+  document.getElementById("dummyKeyboard").focus();
+  console.log("Application loaded");
+};
+
+const execute = function executeCommand(input) {
+  let output;
+  input = input.toLowerCase();
+  if (input.length === 0) {
+    return;
+  }
+  output = `<div class="terminal-line"><span class="success">➜</span> <span class="directory">~</span> ${input}</div>`;
+  if (!COMMANDS.hasOwnProperty(input)) {
+    output += `<div class="terminal-line">no such command: ${input}</div>`;
+    console.log("Oops! no such command");
+  } else {
+    output += COMMANDS[input];
+  }
+
+  terminalOutput.innerHTML = `${
+    terminalOutput.innerHTML
+  }<div class="terminal-line">${output}</div>`;
+  terminalOutput.scrollTop = terminalOutput.scrollHeight;
+};
+
+const key = function keyEvent(e) {
+  const input = userInput.innerHTML;
+
+  if (BLACKLISTED_KEY_CODES.includes(e.keyCode)) {
+    return;
+  }
+
+  if (e.key === "Enter") {
+    execute(input);
+    userInput.innerHTML = "";
+    return;
+  }
+
+  userInput.innerHTML = input + e.key;
+};
+
+const backspace = function backSpaceKeyEvent(e) {
+  if (e.keyCode !== 8 && e.keyCode !== 46) {
+    return;
+  }
+  userInput.innerHTML = userInput.innerHTML.slice(
+    0,
+    userInput.innerHTML.length - 1
+  );
+};
+
+document.addEventListener("keydown", backspace);
+document.addEventListener("keypress", key);
+document.addEventListener("DOMContentLoaded", app);
